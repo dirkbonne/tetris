98a2abc0da3096b857c4fdace74f04a678a193d4
Dirk Bonné <dirk.bonne@gmail.com>
2017-06-15 15:13:39 +0200

    More refactorings

--- app/figures.js
+++ app/figures.js
@@ -1,6 +1,6 @@
-var c = require("./coord.js");
+import c from "./coord.js";
 
-var Figure = function() {
+let Figure = function() {
     this.rotations = [];
 };
 
@@ -8,21 +8,17 @@ Figure.prototype.getRotation = function (rot) {
     return this.rotations[rot % this.rotations.length ];
 };
 
-// the originPostitions denote the placement of the figure relative to
-// the previous rotation form. Multiple are posible to allow for some
-// wiggling when there are obstructing stones in the field 
+// the originPositions allows to place the brick at multiple locations
+// to allow for some wiggling to fit into the board.
 Figure.prototype.addRotations = function(stoneList, originPositions, n) {
-    var bound = c.boundingBox(stoneList);
+    let bound = c.boundingBox(stoneList);
     let w = bound.x + bound.w;
     let h = bound.y + bound.h;
 
     this.rotations.push({ stoneList, originPositions, w, h});
     // calculate the other rotations:
     for(let i = 1; i < n; ++i) {
-	// swap w and h
-	let tmp = w;
-	w = h;
-	h = tmp;
+	[w, h] = [h, w];
 	stoneList = stoneList.map(cd => c.make(cd.y, h - 1 - cd.x)); // jshint ignore:line
 	originPositions = originPositions.map(cd => c.make(-cd.x, cd.y)); // jshint ignore:line
 	this.rotations.push({ stoneList, originPositions, w, h});
@@ -37,57 +33,55 @@ Figure.prototype.setImageSrc = function (imageSrc) {
 };
 
 Figure.prototype.makeMirror = function () {
-    var m = new Figure();
-    var r = this.rotations[0];
-    var w = r.w;
+    let m = new Figure();
+    let r = this.rotations[0];
+    let w = r.w;
     m.addRotations(r.stoneList.map(p => c.make(w - 1 - p.x, p.y)), 
 		   r.originPositions, this.rotations.length);
 
     return m;
 };
 
-var wiggle2x3 = [c.make(1,0), c.make(0,0)];
-var wiggle3x2 = wiggle2x3.map(cd => c.make(-cd.x, cd.y));
-var nowiggle = [c.make(0,0)];
-var wiggle4x1 = [c.make(-1,0), c.make(-2,0), c.make(0,0), c.make(-3,0)];
+let wiggle3x2 = [c.make(-1,0), c.make(0,0)];
+let nowiggle = [c.make(0,0)];
+let wiggle4x1 = [c.make(-1,0), c.make(-2,0), c.make(0,0), c.make(-3,0)];
 
-// xx
-// x
-// x
-var rightHookFigure = new Figure()
+// xxx
+//   x
+let rightHookFigure = new Figure()
     .setImageSrc("images/bluestone.jpg")
-    .addRotations([c.make(0,0), c.make(0,1), c.make(0,2), c.make(1,2)],
-		  wiggle2x3, 4);
+    .addRotations([c.make(0,1), c.make(1,1), c.make(2,1), c.make(2,0)],
+		  wiggle3x2, 4);
 
 //  xx
 // xx
-var rightStepFigure = new Figure()
+let rightStepFigure = new Figure()
     .setImageSrc("images/greenstone.jpg")
     .addRotations([c.make(0,0), c.make(1,0), c.make(1,1), c.make(2,1)],
 		  wiggle3x2, 2);
 
 // xxx
 //  x
-var tFigure = new Figure()
+let tFigure = new Figure()
     .setImageSrc("images/magentastone.jpg")
     .addRotations([c.make(0,1), c.make(1,1), c.make(2,1), c.make(1,0)],
 		  wiggle3x2, 4);
 
 // xx
 // xx
-var squareFigure = new Figure()
+let squareFigure = new Figure()
     .setImageSrc("images/yellowstone.jpg")
     .addRotations([c.make(0,0), c.make(1,0), c.make(0,1), c.make(1,1)],
 		  nowiggle, 1);
 
 // xxxx
-var stickFigure = new Figure()
+let stickFigure = new Figure()
     .setImageSrc("images/cyanstone.jpg")
     .addRotations([c.make(0,0), c.make(1,0), c.make(2,0), c.make(3,0)],
 		 wiggle4x1, 2);
 
 
-var Figures = function() {
+let Figures = function() {
     this.figures = [ rightHookFigure,
 		     rightHookFigure.makeMirror().setImageSrc("images/orangestone.jpg"),
 		     rightStepFigure,
